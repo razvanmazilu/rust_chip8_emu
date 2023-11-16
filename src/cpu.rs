ee6862b52c706b0d5e2e9079dc24e4728e6e1546
Razvan Mazilu <rmazilu@gmail.com>
2023-11-16 23:22:32 +0200

    added more instructions. add debugg_drawing_sprite ; exit

--- src/cpu.rs
+++ src/cpu.rs
@@ -44,14 +44,43 @@ impl Cpu {
 
 		match (instruction & 0xF000) >> 12{
 			0x1 =>{
-				//go to nnn
+				//go to nnn;
 				self.pc = nnn;
+			},
+			0x3 => {
+				// skip next instruction if vx = nn;
+				if(self.read_reg_vx(x) == nn.try_into().unwrap()) {
+					self.pc += 4;
+				} else {
+					self.pc += 2;
+				}
+
 			}
-			0x6 =>{
+			0x6 =>  {
 				//vx = nn;
 				self.write_reg_vx(x, nn.try_into().unwrap());
 				self.pc += 2;
-
+			},
+			0x7 => {
+				//vx = vx + nn;
+				let vx = self.read_reg_vx(x);
+				self.write_reg_vx(x, vx.wrapping_add(nn.try_into().unwrap()));
+				self.pc += 2;
+			}
+			0xD => {
+				//draw(x,y,n);
+				self.debug_draw_sprite(ram, x.try_into().unwrap(), y.try_into().unwrap(),n.try_into().unwrap());
+				self.pc += 2;
+			},
+			0xA => {
+				// i = nnn;
+				self.i = nnn;
+				self.pc += 2;
+			},
+			0xF => {
+				// i +=vx;
+				self.i += self.read_reg_vx(x) as u16;
+				self.pc += 2;
 			}
 
 			_=>panic!("unrecognized instruction {:#X} : {:#X}", self.pc, instruction)
@@ -59,6 +88,22 @@ impl Cpu {
 
     }
 
+	fn debug_draw_sprite(&self, ram: &mut Ram, x: u8, y: u8, height: u8) {
+		println!("Draw sprite at ({}, {})", x, y);
+		for y in 0..height {
+			let mut b = ram.read_byte(self.i + y as u16);
+			for _ in 0..8 {
+				match (b & 0b1000_0000)  >> 7{
+					0 => print!(" "),
+					1 => print!("#"),
+					_ => unreachable!(),
+				}
+				b = b << 1;
+			}
+			print!("\n");
+		}
+		print!("\n");
+	}
 	pub fn write_reg_vx(&mut self, index: u16, value: u8) {
 		self.vx[index as usize] = value;
 	}
